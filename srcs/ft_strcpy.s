35fc7399afbb8a68653a358de1f40246bcab0186
awerebea <awerebea.21@gmail.com>
2020-09-04 14:07:27 +0300

    ft_strcpy added

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -6,7 +6,7 @@
 #    By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/03 12:32:13 by awerebea          #+#    #+#              #
-#    Updated: 2020/09/03 19:00:28 by awerebea         ###   ########.fr        #
+#    Updated: 2020/09/04 14:05:33 by awerebea         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,13 +15,13 @@ section	.text
 
 _ft_strcpy:
 	xor		rcx, rcx
+	mov		rax, rdi
 .loop:
-	cmp		byte[rsi + rcx], 0;
-	je		.end
-	mov		[rdi + rcx], byte[rsi + rcx]
+	mov		dl, byte[rsi + rcx]
+	mov		byte[rdi + rcx], dl
+	test	dl, dl;
+	jz		.end
 	inc		rcx
 	jmp		.loop
 .end
-	mov		[rdi + rcx + 1], 0
 	ret
-
